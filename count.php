f7dc8b9ec710b56a4c18d89e7bed3831df73fd0d
AmujoDotun <samuelamujo@gmail.com>
2020-07-24 14:39:43 +0100

    count function count the number of words that are in an array

---
+++ count.php
@@ -0,0 +1,17 @@
+<?php
+
+$array = array('mango', 'orange', 'apple', 'pennapple', 'pea');
+echo count($array, COUNT_NORMAL);
+die();
+
+// is_array
+// if(is_array($array))
+// {
+//     for($i=0; $i<count($array); $i++)
+//     {
+//         echo $array[$i]. '<br>';
+//     }
+// }
+// else{
+//     echo "It's not an array";
+// }
