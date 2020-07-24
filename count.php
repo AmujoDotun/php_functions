5442ea2fb05fb7f4c182bdc8cef7ec1959d2b6f7
AmujoDotun <samuelamujo@gmail.com>
2020-07-24 14:44:38 +0100

    is_array function check if string passed is an array of not

--- count.php
+++ count.php
@@ -2,16 +2,17 @@
 
 $array = array('mango', 'orange', 'apple', 'pennapple', 'pea');
 echo count($array, COUNT_NORMAL);
-die();
+// die();
 
 // is_array
-// if(is_array($array))
-// {
-//     for($i=0; $i<count($array); $i++)
-//     {
-//         echo $array[$i]. '<br>';
-//     }
-// }
-// else{
-//     echo "It's not an array";
-// }
+if(is_array($array))
+{
+    // count function used here is to prevent unlimited looping which will brake the system
+    for($i=0; $i<count($array); $i++)
+    {
+        echo $array[$i]. '<br>';
+    }
+}
+else{
+    echo "It's not an array";
+}
