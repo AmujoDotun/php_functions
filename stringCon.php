0eebd1cf6859a4ac14dbfe5af0e2e1e91bdcbefb
AmujoDotun <samuelamujo@gmail.com>
2020-07-24 14:55:20 +0100

    substr function extracts word from a sentences indicating where it should start from and where it should end

---
+++ stringCon.php
@@ -0,0 +1,6 @@
+<?php
+// this function extract words form a sentene by indication the starting point
+$text ="This is the main guy in the building";
+$extract = substr($text, 0,4);
+
+echo $extract;
