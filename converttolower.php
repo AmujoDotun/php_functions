1e6578571379a2e75e85b5472e9f8806ab229aff
AmujoDotun <samuelamujo@gmail.com>
2020-08-20 19:23:41 +0100

    Convert strings in compital letters to lower case

---
+++ converttolower.php
@@ -0,0 +1,10 @@
+<?php
+
+$test ="I HAVE BEING TELLING YOU FOR DAYS THAT YOU SHOULD BE GENTLE BUT YOU WON'T LISTER";
+
+$car = "We have different type of cars, it all depends on which one you want";
+
+
+echo strtolower($test);
+
+echo strtolower($car);
